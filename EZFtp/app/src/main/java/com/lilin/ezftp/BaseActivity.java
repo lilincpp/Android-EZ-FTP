b414183c9410db08f3eb7348babdf0ce461e8904
Colin <lilincpp@gmail.com>
2020-08-04 15:44:00 +0800

    Add files via upload

--- EZFtp/app/src/main/java/com/lilin/ezftp/BaseActivity.java
+++ EZFtp/app/src/main/java/com/lilin/ezftp/BaseActivity.java
@@ -1,6 +1,13 @@
 package com.lilin.ezftp;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
