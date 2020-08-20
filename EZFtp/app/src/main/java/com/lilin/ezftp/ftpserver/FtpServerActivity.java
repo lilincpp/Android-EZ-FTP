3432bb6e9991331bec8568fefde125a2ce35cef0
colin <lilincpp@gmail.com>
2020-08-20 10:30:06 +0800

    1. Support browser connect ftp server and login

--- EZFtp/app/src/main/java/com/lilin/ezftp/ftpserver/FtpServerActivity.java
+++ EZFtp/app/src/main/java/com/lilin/ezftp/ftpserver/FtpServerActivity.java
@@ -24,8 +24,6 @@ import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author lilin
